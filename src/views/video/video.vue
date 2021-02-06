685060a98e8b29a536a015cf414a38c20dc5041c
never9920 <929654604@qq.com>
2021-02-06 23:46:05 +0800

    video(1)

--- src/views/video/video.vue
+++ src/views/video/video.vue
@@ -10,28 +10,28 @@
           <span class="first">{{ model.category.title }}</span>
           <span>{{ model.name }}</span>
         </div>
-        <div class="detailfo">
+        <div v-if="model.userinfo" class="detailfo">
           <span class="third">{{ model.userinfo.name }}</span>
           <span class="second">168万次播放</span>
           <span class="second">6688弹幕</span>
           <span class="second">{{ model.date }}</span>
         </div>
         <div class="icons">
-            <div @click="show1=!show1">
+          <div @click="show1 = !show1">
             <img v-if="show1" src="~assets/img/shou.svg" />
-            <img v-else src="~assets/img/shoupink.svg">
+            <img v-else src="~assets/img/shoupink.svg" />
             <span>收藏</span>
-            </div>
-            <div @click="show2=!show2">
-            <img v-if="show2" src="~assets/img/xia.svg">
-            <img v-else src="~assets/img/xiapink.svg">
+          </div>
+          <div @click="show2 = !show2">
+            <img v-if="show2" src="~assets/img/xia.svg" />
+            <img v-else src="~assets/img/xiapink.svg" />
             <span>下载</span>
-            </div>
-            <div @click="show3=!show3">
-            <img v-if="show3" src="~assets/img/fen.svg">
-            <img v-else src="~assets/img/fenpink.svg">
+          </div>
+          <div @click="show3 = !show3">
+            <img v-if="show3" src="~assets/img/fen.svg" />
+            <img v-else src="~assets/img/fenpink.svg" />
             <span>分享</span>
-            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -46,9 +46,9 @@ export default {
     return {
       imgsrc: "",
       model: [],
-      show1:true,
-      show2:true,
-      show3:true,
+      show1: true,
+      show2: true,
+      show3: true,
     };
   },
 
@@ -106,25 +106,25 @@ export default {
   border-radius: 10px;
   font-size: 12px;
 }
-.detailfo{
-    padding: 10px;
+.detailfo {
+  padding: 10px;
 }
-.second{
-    color:#aaa;
-    font-size: 12px;
-    padding: 0 10px;
+.second {
+  color: #aaa;
+  font-size: 12px;
+  padding: 0 10px;
 }
-.third{
-    color: black;
-    font-size: 14px;
-    padding-right: 10px;
+.third {
+  color: black;
+  font-size: 14px;
+  padding-right: 10px;
 }
-.icons{
-    display: flex;
-    justify-content: space-around;
+.icons {
+  display: flex;
+  justify-content: space-around;
 }
-.icons img{
-    height: 16px;
-    width: 16px;
+.icons img {
+  height: 16px;
+  width: 16px;
 }
 </style>
